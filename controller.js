afbd7d80823d32757f0980e23a3f87d70a17b11d
Steven Anderson <stevenanderson@Stevens-MacBook-Pro.local>
2016-09-28 13:40:29 -0600

    finished

--- controller.js
+++ controller.js
@@ -1,10 +1,11 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+
+  mainService.getUsers().then(function(response) {
+    $scope.users = response.data.data;
+  });
 
 
-  $scope.getUsers();
+
 
 });
